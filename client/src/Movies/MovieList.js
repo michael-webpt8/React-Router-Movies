46bea49a1bb65330f64fcf900d4d0e9370ba93d1
Michael Williams <manicdesigner@gmail.com>
2019-09-04 18:41:54 -0400

    add Link wrapping MovieDeatils Component for movies/movie.id

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -13,19 +14,21 @@ const MovieList = props => {
         .catch(error => {
           console.error('Server Error', error);
         });
-    }
-    
+    };
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`movies/${movie.id}`} key={movie.id}>
+          <MovieDetails key={movie.id} movie={movie} />{' '}
+        </Link>
       ))}
     </div>
   );
-}
+};
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
